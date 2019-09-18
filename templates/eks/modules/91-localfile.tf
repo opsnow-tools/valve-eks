c566da533efc7a3fbd0d2d3c675d8f115cbae42a
longlg88 <longlg88@naver.com>
2019-09-18 03:59:14 +0000

    modify local file path

--- templates/eks/modules/91-localfile.tf
+++ templates/eks/modules/91-localfile.tf
@@ -90,17 +90,17 @@ data "template_file" "aws_auth" {
 
 resource "local_file" "aws_auth" {
   content  = data.template_file.aws_auth.rendered
-  filename = "${path.cwd}/.output/aws_auth.yaml"
+  filename = "/terraformtmp/.output/aws_auth.yaml"
 }
 
 resource "local_file" "kube_config" {
   content  = data.template_file.kube_config.rendered
-  filename = "${path.cwd}/.output/kube_config.yaml"
+  filename = "/terraformtmp/.output/kube_config.yaml"
 }
 
 resource "local_file" "kube_config_secret" {
   content  = data.template_file.kube_config_secret.rendered
-  filename = "${path.cwd}/.output/kube_config_secret.yaml"
+  filename = "/terraformtmp/.output/kube_config_secret.yaml"
 }
 
 resource "null_resource" "executor" {
