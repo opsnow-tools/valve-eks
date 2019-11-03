3f8a217b123caa07c0e29c35e1cc6dc5e02af010
gelius <gelius7@gmail.com>
2019-11-03 15:38:25 +0900

    EFS output fix
    import 할때 사용할 수 있도록 output 수정

--- modules/efs/outputs.tf
+++ modules/efs/outputs.tf
@@ -1,3 +1,15 @@
 output "efs_id" {
     value = aws_efs_file_system.efs.id
 }
+
+output "efs_mount_target_ids" {
+    value = aws_efs_mount_target.efs.*.id
+}
+
+output "aws_security_group_id" {
+    value = aws_security_group.efs.id
+}
+
+output "aws_security_group_rule_import_param" {
+    value = "${aws_security_group.efs.id}_${aws_security_group_rule.efs-ingress-worker.type}_${aws_security_group_rule.efs-ingress-worker.protocol}_${aws_security_group_rule.efs-ingress-worker.from_port}_${aws_security_group_rule.efs-ingress-worker.to_port}_${data.aws_security_group.mount_target_sg.id}"
+}
