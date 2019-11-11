84ba26722924dac043b33341fa09c84c870a1d1e
gelius <gelius7@gmail.com>
2019-11-11 19:29:18 +0900

    migration variable example update

--- example/eks-4steps-greenset/3-eks-network/import.sh
+++
@@ -1,5 +0,0 @@
-
-terraform import module.eks-domain.aws_route53_record.validation Z1PY2EID2YMYG4__df533acea0ac6b1093d8e0533073dc3a.jj1.opsnow.io._CNAME
-
-
-terraform import module.eks-domain.aws_route53_record.validation_alt1 Z1PY2EID2YMYG4__10c9e3c9608e4ce3bb170dc52b9134e4.jj0.opsnow.io._CNAME
