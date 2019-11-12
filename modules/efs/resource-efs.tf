64c77539005b76c911e37b15c2cb2e6c4ff888bd
gelius <gelius7@gmail.com>
2019-11-12 18:15:30 +0900

    Revert "EFS name to lower"
    This reverts commit 44cf16d36be18612ead133ba97802678c6bb5072.

--- modules/efs/resource-efs.tf
+++ modules/efs/resource-efs.tf
@@ -4,7 +4,7 @@ resource "aws_efs_file_system" "efs" {
   creation_token = "${local.lower_name}"
 
   tags = {
-    "Name"                                      = "${local.lower_name}-EFS"
+    "Name"                                      = "${local.upper_name}-EFS"
     "KubernetesCluster"                         = local.lower_name
   }
 }
